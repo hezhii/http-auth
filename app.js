53eccaf212d1a4625fbc5aa96042e1ddb792d89f
biu <hezhou920@gmail.com>
2018-04-02 23:28:47 +0800

    basic auth

--- app.js
+++ app.js
@@ -9,6 +9,33 @@ app.get('/', (req, res) => {
   })
 })
 
+app.get('/basic', (req, res) => {
+  const unauthorized = (res) => {
+    res.set('WWW-Authenticate', 'Basic realm="Please input username and password"')
+    res.sendStatus(401)
+  }
+  const parse = (header) => {
+    if (typeof header !== 'string') {
+      return
+    }
+    const str = Buffer.from(header.substring(6), 'base64').toString()
+    const i = str.indexOf(':')
+    return {
+      name: str.substring(0, i),
+      password: str.substring(i + 1)
+    }
+  }
+  const authorization = req.get('authorization')
+  if (!authorization) {
+    return unauthorized(res)
+  }
+  const user = parse(authorization)
+  if (user && user.name === 'admin' && user.password === '123456') {
+    return res.send(`<h1>Welcome, ${user.name}</h1>`)
+  }
+  unauthorized(res)
+})
+
 const server = http.createServer(app)
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => {
